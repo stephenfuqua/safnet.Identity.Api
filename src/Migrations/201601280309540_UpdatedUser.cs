585fbc93fef1b3897cfd9139481096a914548cb8
kpunjani <kpunjani@gmail.com>
2016-01-28 17:46:25 +0100

    Fixed the SQL Script to update the users active status

--- src/Migrations/201601280309540_UpdatedUser.cs
+++ src/Migrations/201601280309540_UpdatedUser.cs
@@ -7,10 +7,10 @@ namespace FlightNode.Identity.Migrations
         public override void Up()
         {
             AlterColumn("dbo.Users", "Active", c => c.String());
-
-            base.Sql("UPDATE dbo.Users SET CASE WHEN Active = 1 THEN 'active' ELSE 'inactive'");
+            
+            base.Sql("UPDATE dbo.Users SET Active = CASE WHEN Active = 1 THEN 'active' ELSE 'inactive' END");
         }
-        
+
         public override void Down()
         {
             AlterColumn("dbo.Users", "Active", c => c.Boolean(nullable: false));
