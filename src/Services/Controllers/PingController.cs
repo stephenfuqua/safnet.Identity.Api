7c3628992cbd33da794c5dbc7270ae01c4a0a152
Stephen Fuqua <stephen@safnet.com>
2017-06-17 22:26:56 -0500

    New endpoint for waking up the service

---
+++ src/Services/Controllers/PingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Web.Http;
+
+namespace FlightNode.Identity.Services.Controllers
+{
+    /// <summary>
+    /// Returns the current date time, thus confirming that the API service is running
+    /// </summary>
+    public class PingController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            return Ok(DateTime.Now.ToString());
+        }
+    }
+}
