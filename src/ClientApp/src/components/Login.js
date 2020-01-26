38c0998f25df36c20066cf1082ec580242a61244
Stephen Fuqua <stephen@safnet.com>
2020-01-26 16:55:00 -0600

    Form with event handlers

--- src/ClientApp/src/components/Login.js
+++ src/ClientApp/src/components/Login.js
@@ -1,35 +1,51 @@
 import React from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import { actionCreators } from '../store/Login';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const Login = props => (
-    <Container>
-        <h1>Sign-in</h1>
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            emailAddress: '',
+            password: '',
+            rememberMe: false
+        };
+    }
 
-        <Form>
-            <Form.Group controlId="email-address">
-                <Form.Label>E-mail Address</Form.Label>
-                <Form.Control type="email" placeholder="E-mail address / username" />
-            </Form.Group>
-            <Form.Group controlId="password">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="remember-me">
-                <Form.Check type="checkbox" label="Remember My Login" />
-            </Form.Group>
-            <Form.Group>
-                <Button variant="primary" type="submit">Login</Button>
-            </Form.Group>
-        </Form>
-    </Container>
-);
+    signIn = event => {
+        event.preventDefault();
+        alert('hello ' + this.state.emailAddress);
+    }
 
-export default connect(
-    state => state.counter,
-    dispatch => bindActionCreators(actionCreators, dispatch)
-)(Login);
+    formChangeHandler = event => {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({ [nam]: val });
+    }
+
+    render() {
+        return <Container>
+            <h1>Sign-in</h1>
+
+            <Form>
+                <Form.Group controlId="email-address">
+                    <Form.Label>E-mail Address</Form.Label>
+                    <Form.Control type="email" placeholder="E-mail address / username" name="emailAddress" onChange={this.formChangeHandler} required />
+                </Form.Group>
+                <Form.Group controlId="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" name="password" onChange={this.formChangeHandler} required />
+                </Form.Group>
+                <Form.Group controlId="remember-me">
+                    <Form.Check type="checkbox" label="Remember My Login" name="rememberMe" onChange={this.formChangeHandler} />
+                </Form.Group>
+                <Form.Group>
+                    <Button variant="primary" type="submit" onSubmit={this.signIn} onClick={this.signIn}>Login</Button>
+                </Form.Group>
+            </Form>
+        </Container>
+    }
+}
+
+export default Login;
